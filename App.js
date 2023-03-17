6a3f3b2f49a56a5c502c9f586a7463c6ca4d2a98
sasikumar-07 <128038363+sasikumar-07@users.noreply.github.com>
2023-03-17 10:20:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+console.log("Message to Developer")
+function Alertmessage()
+{
+const Hi =()=>{alert("Message from Javascript Alert!")}
+return <div><button onClick={Hi}>Click</button></div>
+}
+ReactDOM.render(<Alertmessage/>,document.getElementById('mydiv'))
